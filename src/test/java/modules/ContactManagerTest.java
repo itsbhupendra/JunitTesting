8db66b9f6d36eba0e0852cc42bec60526cb8ba2a
bhupendrasingh <bhupendrasingh12398@gmail.com>
2021-06-26 17:51:25 +0530

    Implemented Junit Test Lifecycle

--- src/test/java/modules/ContactManagerTest.java
+++ src/test/java/modules/ContactManagerTest.java
@@ -1,17 +1,24 @@
 package modules;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ContactManagerTest {
+    public ContactManager contactManager;
+
+    @BeforeAll
+    public static void setupAll(){
+        System.out.println("Before All");
+    }
+    @BeforeEach
+    public void setup(){
+        contactManager=new ContactManager();
+    }
 
     @Test
     public void shouldCreateContact(){
-        ContactManager contactManager = new ContactManager();
+//        ContactManager contactManager = new ContactManager();
         contactManager.addContact("Bhupendra","Singh","0986754768");
         Assertions.assertFalse(contactManager.getAllContacts().isEmpty());
         Assertions.assertEquals(1,contactManager.getAllContacts().size());
@@ -20,11 +27,19 @@ class ContactManagerTest {
     @Test
     @DisplayName("Should Not create contact when first name is null")
     public void NullFirstName (){
-        ContactManager contactManager =new ContactManager();
+//        ContactManager contactManager =new ContactManager();
         Assertions.assertThrows(RuntimeException.class,()->{
             contactManager.addContact(null,"Singh","0123456789");
         });
 
+    }
+    @AfterEach
+    public void tearDown(){
+        System.out.println("After Each");
+    }
+    @AfterAll
+    public static void tearDownAll(){
+        System.out.println("After All");
     }
 
 }
